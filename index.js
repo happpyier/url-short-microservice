3dd9d3a61cdbc6f5b9e57b0a9162c401925f7bb8
Thomas Bernfeld <happpyier@gmail.com>
2016-03-07 14:06:53 -0500

    From work solving the forward slash problem

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
 var express = require('express');
 var app = express();
-app.use(express.bodyParser());
 var path = require("path");
 app.set('port', (process.env.PORT || 5000));
 
-app.post('/:tagId', function(request, response) {
+app.get('/:tagId', function(request, response) {
   var dummyVar = Object.keys(request);
-  var dummyVarTest = request.url;
+  var dummyVarTest = request.query;
   var dummyVarTestStringified = JSON.stringify(request.url);
-  var preReponseParsed = 'request.params.tagId';
+  var preReponseParsed = request.params.tagId;
   response.send(dummyVar+'<br/> dummyVarTest--->'+dummyVarTest+'<br/> dummyVarTestStringified--->'+dummyVarTestStringified+'<br/><br/>'+'Your orignal website is<br/>'+preReponseParsed);
 });
 
-app.post('/', function(request, response) {
+app.get('/', function(request, response) {
   response.sendFile(path.join(__dirname+'/index.html'));
   //response.end('Its Over!'); 
 });
-app.listen(app.post('port'), function() {
+app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
