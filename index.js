82bd7b698ab9961ff233b72630ed89ec3dd9679c
Thomas Bernfeld <happpyier@gmail.com>
2016-03-07 13:51:13 -0500

    From work solving the forward slash problem

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-/*
 var express = require('express');
 var app = express();
 var path = require("path");
@@ -19,21 +18,3 @@ app.get('/', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-*/
-var http = require('http');
-
-http.createServer(function(request, response) {
-  var headers = request.headers;
-  var method = request.method;
-  var url = request.url;
-  var body = [];
-  request.on('error', function(err) {
-    console.error(err);
-  }).on('data', function(chunk) {
-    body.push(chunk);
-  }).on('end', function() {
-    body = Buffer.concat(body).toString();
-    // At this point, we have the headers, method, url and body, and can now
-    // do whatever we need to in order to respond to this request.
-  });
-}).listen(8080); // Activates this server, listening on port 8080.
