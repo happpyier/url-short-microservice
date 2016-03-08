84a1dad35dbb304ea75a70a53039f8a34d5fbbe9
Thomas Bernfeld <happpyier@gmail.com>
2016-03-08 15:58:12 -0500

    From work getting the address value to load

--- index.js
+++ index.js
@@ -1,37 +1,17 @@
-
 var express = require('express');
 var app = express();
 var path = require("path");
 var url = require("url");
+app.set('port', (process.env.PORT || 5000));
 
-
-/*
-app.get('/:tagId', function(request, response) {
+app.get('/:url', function(request, response) {
   var dummyVar = Object.keys(request);
   var dummyVarTest = request.query;
   var dummyVarTestStringified = JSON.stringify(request.url);
-  var preReponseParsed = request.params.tagId;
+  var preReponseParsed = request.url;
   response.send(dummyVar+'<br/> dummyVarTest--->'+dummyVarTest+'<br/> dummyVarTestStringified--->'+dummyVarTestStringified+'<br/><br/>'+'Your orignal website is<br/>'+preReponseParsed);
 });
 
-app.get('/', function(request, response) {
-  response.sendFile(path.join(__dirname+'/index.html'));
-  //response.end('Its Over!'); 
-});
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
-*/
-var fs = require('fs');
-var http = require('http');
-//var url = require('url') ;
-app.set('port', (process.env.PORT || 5000));
-app.createServer('/:tagId', function (request, response) {
-  var queryObject = url.parse(request.url,true).query;
-  console.log(queryObject);
-  response.writeHead(200);
-  response.end('Feel free to add query parameters to the end of the url');
-});
 app.get('/', function(request, response) {
   response.sendFile(path.join(__dirname+'/index.html'));
   //response.end('Its Over!'); 
