8a1dda09f20c9bcf6acc07bb3396f40e2c11b4b2
Thomas Bernfeld <happpyier@gmail.com>
2016-03-09 14:10:09 -0500

    From work Getting the URL to appear in the browser

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+
 var express = require('express');
 var app = express();
+/*
 var path = require("path");
 var url = require("url");
 app.set('port', (process.env.PORT || 5000));
@@ -20,3 +22,17 @@ app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 //LOOK INTO USING MONGODB, looking at this project from the wrong direction.
+*/
+var pg = require('pg');
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/db', {results: result.rows} ); }
+    });
+  });
+})
