70554c9f9f9982602be2944865dbbe7ce46e7397
Thomas Bernfeld <happpyier@gmail.com>
2016-03-09 11:50:22 -0500

    From work Getting the URL to appear in the browser

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var path = require("path");
 var url = require("url");
 app.set('port', (process.env.PORT || 5000));
 
-app.get('/:url', function(escape(request), escape(response)) {
+app.get('/:url', function(escape(request), response) {
   var dummyVar = Object.keys(request);
   var dummyVarTest = request.query;
   var dummyVarTestStringified = JSON.stringify(request.url);
