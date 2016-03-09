46099fc7e572ee34b6f4669a2155a61e4e302eff
Thomas Bernfeld <happpyier@gmail.com>
2016-03-09 11:53:48 -0500

    From work Getting the URL to appear in the browser

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ var url = require("url");
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/:url', function(request, response) {
+	/*
   var dummyVar = Object.keys(request);
   var dummyVarTest = request.query;
   var dummyVarTestStringified = JSON.stringify(request.url);
   var preReponseParsed = (request.url).replace(/%2F/g, "/");
   response.send(dummyVar+'<br/> dummyVarTest--->'+dummyVarTest+'<br/> dummyVarTestStringified--->'+dummyVarTestStringified+'<br/><br/>'+'Your orignal website is<br/>'+preReponseParsed);
+  */
 });
 
 app.get('/', function(request, response) {
