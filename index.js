3147ea162b9f08b286407b0e4566793709699196
Thomas Bernfeld <happpyier@gmail.com>
2016-03-10 08:40:26 -0500

    From work figuring out the forward slashes problem

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var path = require("path");
 var url = require("url");
 app.set('port', (process.env.PORT || 5000));
 app.set({ 'content-type': 'text; charset=utf-8' })
-app.get('/:url=', function(request, response) {
+app.get('/:url', function(request, response) {
   encodeURIComponent(url);
   var dummyVar = typeof(request);
   var dummyVarTest = request.query;
