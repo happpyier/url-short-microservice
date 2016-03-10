b00c871c2ade7a4b91ba4a4459c143de858d6c2e
Thomas Bernfeld <happpyier@gmail.com>
2016-03-10 15:21:45 -0500

    From Work working on the slashes

--- index.js
+++ index.js
@@ -33,9 +33,12 @@ app.get('/db', function (request, response) {
 });
 
 //app.param('/:tagId', convertURL, next, id);
-app.param('/:tagId', function (request, response, tagId) {
-  preUrlValue = tagId;
+app.param('id', function (request, response, next, id) {
+  preUrlValue = id;
   UrlValue = preUrlValue.replace(/\//g, "%2F");
+  
+});
+app.get(':id', function (request, response) {
   response.send(UrlValue);
 });
 app.listen(app.get('port'), function() {
