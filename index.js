75d5e2a9545ed1b8805aecaa35c93ae055d11e9a
Thomas Bernfeld <happpyier@gmail.com>
2016-03-11 10:53:46 -0500

    From work Working out the slashes

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.param('url', function (request, response, next, id) {
   response.end("it ended!");
   next();
 });
-app.all('/:url', function (req, res, next) {  
+app.param('/:url', function (req, res, next) {  
   console.log('Someone made a request!');
   next();
 });
