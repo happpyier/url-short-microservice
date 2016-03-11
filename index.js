33f26f0c911e46f453e68ae95ba749a223e4e96a
Thomas Bernfeld <happpyier@gmail.com>
2016-03-11 14:18:07 -0500

    From work Working out the slashes

--- index.js
+++ index.js
@@ -24,17 +24,6 @@ app.get('/', function(request, response) {
   response.sendFile(path.join(__dirname+'/index.html'));
   //response.end('Its Over!'); 
 });
-app.get('/db', function (request, response) {
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM test_table', function(err, result) {
-      done();
-      if (err)
-       { console.error(err); response.send("Error " + err); }
-      else
-       { response.render('pages/db', {results: result.rows} ); }
-    });
-  });
-});
 app.get(/^\/http/i, function (request, response) {
   var OrignalHttp = (request.url).substring(1);
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
