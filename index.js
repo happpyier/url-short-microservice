c4e18d6ec4ec80300fd8f16755134fe99fa827c4
Thomas Bernfeld <happpyier@gmail.com>
2016-03-11 15:05:11 -0500

    From Work Getting the Database working

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ var resultsSQL = "";
 var convertURL = function (request, response, next) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
     client.query('SELECT * FROM test_table', function(err, result) {
-      done();
       if (err)
        { console.error(err); response.send("Error " + err); resultsSQL = 'fail'; }
       else
        { response.render('pages/db', {results: result.rows} ); resultsSQL = 'pass';}
+	   done();
     });
   });
   next();
