dd09a2b635a8cd3108ca544f11d2c4bfb3162516
Thomas Bernfeld <happpyier@gmail.com>
2016-03-11 10:05:16 -0500

    From work Working out the slashes

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ app.param('url', function (request, response, next, id) {
   
 });
 app.get('/:url', function (request, response) {
-  response.send(UrlValue);
+  //response.send(UrlValue);
   response.end("it ended!");
 });
 app.listen(app.get('port'), function() {
