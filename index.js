b4ebb67f1bbb8dfa0a7766240c308674038e2b30
Thomas Bernfeld <happpyier@gmail.com>
2016-03-15 15:08:14 -0400

    From Work working with redirect

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ var redirect1 = function (request, response, next) {
 var redirect2 = function (request, response){
   var PrepageRedirectLocation = redirectresultsSQL.replace(/&colon/g, ':');
   //response.redirect(PrepageRedirectLocation);
-  response.redirect('http://www.google.com');
+  response.redirect(PrepageRedirectLocation);
 };
 var sendInfoToDB1 = function (request, response, next) {
   var OrignalHttpForUse = (request.url).substring(5);
