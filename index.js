6042b38adc11f93f927e06bb54d20845fd815ff8
Thomas Bernfeld <happpyier@gmail.com>
2016-03-15 09:36:43 -0400

    From Work Lets Redirect

--- index.js
+++ index.js
@@ -43,9 +43,9 @@ var getInfoFromDB2 = function (request, response){
 var redirect1 = function (request, response, next) {
   
   var OrignalHttpForUse = (request.params.id);
-  var mysqlID = parseInt(resultsidSQL)+1;
+    /*
   var mysqlOrignalHttpForUse = OrignalHttpForUse.replace(/&/g, '&amp').replace(/</g, '&lt').replace(/>/g, '&gt').replace(/"/g, '&quot').replace(/:/g, '&colon');
-  /*
+
   pg.connect(process.env.DATABASE_URL, function(err, client, done) { 
   client.query("SELECT original_url, short_url FROM url_short_microservice WHERE short_url='"+mysqlOrignalHttpForUse+"'", function(err, result) {
       if (err)
