de84ecb3d4294e9faa4d17075b66e89d1bbebcd8
Thomas Bernfeld <happpyier@gmail.com>
2016-03-15 11:34:32 -0400

    From Work Testing out the basics

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.set('port', (process.env.PORT || 5000));
 app.set("Content-Type", "text/html");
 app.get('/', function(request, response) {
   response.sendFile(path.join(__dirname+'/index.html'));
-  //response.end('Its Over!'); 
+  response.end('Its Over!'); 
 });
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
