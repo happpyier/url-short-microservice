8681231b88dc0a4630a3470f2e159a2446ee77c2
Thomas Bernfeld <happpyier@gmail.com>
2016-03-15 12:03:13 -0400

    From Work Testing out the basics

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ var getInfoFromDB2 = function (request, response){
   //response.end();
 };
 var redirect1 = function (request, response, next) {
-  var mysqlOrignalHttpForUse = 'https&colon//url-short-microservice.herokuapp.com'+request.params.id;
+  var mysqlOrignalHttpForUse = 'https&colon//url-short-microservice.herokuapp.com/'+request.params.id;
   //var mysqlOrignalHttpForUse = OrignalHttpForUse.replace(/&/g, '&amp').replace(/</g, '&lt').replace(/>/g, '&gt').replace(/"/g, '&quot').replace(/:/g, '&colon');
   pg.connect(process.env.DATABASE_URL, function(err, client, done) { 
   client.query("SELECT original_url, short_url FROM url_short_microservice WHERE short_url='"+mysqlOrignalHttpForUse+"'", function(err, result) {
