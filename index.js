595dedb29d053fae17577d5abd8c4476ce5cfe8a
Thomas Bernfeld <happpyier@gmail.com>
2016-03-15 13:37:26 -0400

    From Work working with redirect

--- index.js
+++ index.js
@@ -48,6 +48,7 @@ var getInfoFromDB2 = function (request, response){
 var redirect1 = function (request, response, next) {
   var mysqlOrignalHttpForUse = 'https&colon//url-short-microservice.herokuapp.com/'+request.params.id;
   //var mysqlOrignalHttpForUse = OrignalHttpForUse.replace(/&/g, '&amp').replace(/</g, '&lt').replace(/>/g, '&gt').replace(/"/g, '&quot').replace(/:/g, '&colon');
+  /*
   pg.connect(process.env.DATABASE_URL, function(err, client, done) { 
   client.query("SELECT short_url FROM url_short_microservice WHERE short_url='"+mysqlOrignalHttpForUse+"'", function(err, result) {
       if (err)
@@ -59,6 +60,7 @@ var redirect1 = function (request, response, next) {
 	   done();
     });
   });
+  */
   next();
 };
 var redirect2 = function (request, response){
